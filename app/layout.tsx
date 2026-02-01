b3d373c5edae4fe2efb48bd124b52779fdf0597d
hheo <soyhwan@gmail.com>
2026-02-01 23:48:56 +0900

    style: 데스크톱에서 main flex-1 제거로 footer 위치 조정
    - main: flex-1 lg:flex-initial
    - 데스크톱에서 footer가 콘텐츠 바로 아래 표시

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
           <ThemeProvider>
             <div className="min-h-screen flex flex-col">
               <Header />
-              <main className="flex-1">
+              <main className="flex-1 lg:flex-initial">
                 {children}
               </main>
               <Footer />
